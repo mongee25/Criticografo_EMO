0b38f6e9731f9cab34cb608a526a8e9534d9ae97
Emiliano Monge <mongee587@gmail.com>
2024-10-21 23:00:07 -0700

    Cambio diseño

--- Criticografo_EMO/MainPage.xaml.cs
+++ Criticografo_EMO/MainPage.xaml.cs
@@ -15,39 +15,71 @@ namespace Criticografo_EMO
             InitializeComponent();
         }
 
-    // Método para el botón CRITICAR
-    private void CriticarCommand()
-    {
-        string nombre = eNombre.Text;
-        string genero = ""; // Lógica para obtener el género seleccionado
-                            // Aquí puedes manejar la lógica de las casillas de verificación
-
-        // Ejemplo de mostrar un mensaje
-        DisplayAlert("Crítica", "¡Crítica enviada!", "OK");
-    }
-
-        private void OnCritiqueButtonClicked(object sender, EventArgs e)
+        private void CriticarComando()
         {
+            string nombre = NombreEntry.Text;
+            string genero = ""; 
 
+            
+            DisplayAlert("Crítica", "¡Crítica enviada!", "OK");
         }
 
-        private void OnGenderSelectionChanged(object sender, CheckedChangedEventArgs e)
+        private void Criticar(object sender, EventArgs e)
         {
-            if (HombreRadio.IsChecked)
+            string nombre = NombreEntry.Text;
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                ResultadoLabel.Text = "Por favor ingresa un nombre.";
+                return;
+            }
+
+            string genero = "";
+            if (Hombre.IsChecked)
+                genero = "Hombre";
+            else if (Mujer.IsChecked)
+                genero = "Mujer";
+            else
+            {
+                ResultadoLabel.Text = "Por favor selecciona un género.";
+                return;
+            }
+
+            List<string> atributos = new List<string>();
+            if (AltoCheckBox.IsChecked) atributos.Add(AltoLabel.Text);
+            if (FeoCheckBox.IsChecked) atributos.Add(FeoLabel.Text);
+            if (ListoCheckBox.IsChecked) atributos.Add(ListoLabel.Text);
+            if (GuapoCheckBox.IsChecked) atributos.Add(GuapoLabel.Text);
+            if (RaroCheckBox.IsChecked) atributos.Add(RaroLabel.Text);
+            if (GrandeCheckBox.IsChecked) atributos.Add(GrandeLabel.Text);
+
+            if (atributos.Count == 0)
+            {
+                ResultadoLabel.Text = "Por favor selecciona al menos un atributo.";
+                return;
+            }
+
+          
+            string mensaje = $"Nombre: {nombre}\nGénero: {genero}\nAtributos: {string.Join(", ", atributos)}";
+            ResultadoLabel.Text = mensaje;
+        }
+
+        private void Genero(object sender, CheckedChangedEventArgs e)
+        {
+            if (Hombre.IsChecked)
             {
                 AltoLabel.Text = "Alto";
                 FeoLabel.Text = "Feo";
                 ListoLabel.Text = "Listo";
-                ExtravaganteLabel.Text = "Extravagante";
+                GuapoLabel.Text = "Extravagante";
                 RaroLabel.Text = "Raro";
                 GrandeLabel.Text = "Grande";
             }
-            else if (MujerRadio.IsChecked)
+            else if (Mujer.IsChecked)
             {
                 AltoLabel.Text = "Alta";
                 FeoLabel.Text = "Fea";
                 ListoLabel.Text = "Lista";
-                ExtravaganteLabel.Text = "Extravagante";
+                GuapoLabel.Text = "Extravagante";
                 RaroLabel.Text = "Rara";
                 GrandeLabel.Text = "Grande";
             }
